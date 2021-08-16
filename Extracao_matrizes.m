67d7be08d450811243690509d5a6818fb403679f
Telma Carvalho <telmaclarita@gmail.com>
2021-08-16 16:27:39 -0300

    Correção das matrizes de densidade: traço=1

--- Extracao_matrizes.m
+++ Extracao_matrizes.m
@@ -5,6 +5,6 @@ for n = 1:1001
     %Werner = reshape(W_completo(n, 1:16), 4, 4);
     Werner = W_completo(n, 1:16);
     Werner1 = reshape(Werner, 4, 4);
-    W_original{n} = Werner1;
+    W_original{n,1} = Werner1;
 end
 save('W_original.mat', 'W_original');
