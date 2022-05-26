78be936359f8b8953f074ca9063705afd1a2791f
Telma Carvalho <telmaclarita@gmail.com>
2022-05-26 19:25:10 -0300

    Criação do gráfico comparativo de estados

--- comparacao_estados.m
+++ comparacao_estados.m
@@ -2,23 +2,32 @@ clear all;
 clc;
 
 load('W_parcial_SzOnly_erro.mat');
+load('W_Peso.mat');
 k=1;
 verdadeiro = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
-
+iguais = 0;
 for i=1:1001
-    n=0;
+    contador=0;
     k=i+1;
-    if(i==1001)
+    if(i==1002)
         disp('Comparação finalizada.')
     else
         for j=k:1001
            resposta= W_parcial_SzOnly_erro(i,:) == W_parcial_SzOnly_erro(j,:);
+           contador = contador+1;
            if(resposta==verdadeiro)
                disp('Existem matrizes iguais.')
-           else
-           n = n+1;
+               n= iguais+1;
+               iguais=n;
            end
         end
-        testes(i,1)=n;
+        if(iguais==0)
+            P_Sz(i,1)=1;
+        else
+            P_Sz(i,1)=0;
+        end
+        controle(i,1)=contador;
     end    
 end
+
+save('P_Sz.mat', 'P_Sz');
---
+++ grafico_comparativo_estados.m
@@ -0,0 +1,84 @@
+
+load('P_Sz.mat')
+load('W_Peso.mat')
+y = P_Sz;
+x = W_Peso;
+    
+% for l = 1:size(A, 1) 
+%       if (A(l,1) == 0)
+%         z1 = 1.02;
+%         z11 = 1.34; 
+%         q(l) = A1(l);
+%         r(l) = (z11-z1).*rand(1,1) + z1;
+%     else
+%         q(l) = -1;
+%         r(l) = -1; 
+%     end
+%     if (A(l,1) == 1)
+%         z1 = 1.02;
+%         z11 = 1.34;
+%         u(l) = A1(l);
+%         v(l) = (z11-z1).*rand(1,1) + z1;
+%     else
+%         u(l) = -1;
+%         v(l) = -1;
+%     end
+% end
+% 
+% % Gráfico de erros na fase de classificação
+% x = x(x>-1);
+% y = y(y>-1);
+% z = z(z>-1);
+% i = i(i>-1);
+% 
+% a = a(a>-1);
+% b = b(b>-1);
+% c = c(c>-1);
+% d = d(d>-1);
+% 
+% q = q(q>-1);
+% r = r(r>-1);
+% u = u(u>-1);
+% v = v(v>-1);
+
+figure
+D1 = plot(x,y,'k.', 'MarkerSize', 45);
+% hold on
+% D2 = plot(c, d, 'c.', a, b, 'b.', 'MarkerSize', 45);
+% D3 = plot( u, v, 'c.', q, r, 'b.', 'MarkerSize', 45);
+% hold off
+ set(gca,'FontSize',50)
+ set(gca, 'FontName', 'Times New Roman'); 
+ xticks([0:0.1:1])
+ yticklabels({'Equal states', 'Single state'})
+% y1 = yline(1,'.',' ','LineWidth', 1);
+% % Measurement in Sz and Sy   
+% y2 = yline(1.38,'','{\ita)}');
+% y2.LabelHorizontalAlignment = 'left';
+% set(y2,'FontSize',50, 'FontName', 'Times New Roman   ')
+% y3 = yline(0.5,'.',' ','LineWidth',1);
+% % Measurement in Sz and Sx   
+% y4 = yline(0.86,'','{\itb)}');
+% y4.LabelHorizontalAlignment = 'left';
+% set(y4,'FontSize',50, 'FontName', 'Times New Roman')
+% % Measurement in Sz and partial measurement in Sx (DISCONSIDERADED)
+% % Measurement only in Sz
+% y5 = yline(0.36,'','{\itc)}');
+% y5.LabelHorizontalAlignment = 'left';
+% set(y5,'FontSize',50, 'FontName', 'Times New Roman')
+% 
+% % Fixando um sombreado para P>1/3
+% cm = [0 0 0 ;  0.9 0.9 0.9;  1 1 1];
+% patch([(1/2) (1/2) 1.01 1.01 (1/2)]', [-0 1.5 1.5 -0 -0]', cm(2,:), 'EdgeColor','none', 'DisplayName', 'Entanglement area')
+% axis([-0.02 1.02 -0.02 1.52])
+% set(gca,'children',flipud(get(gca,'children')))
+% 
+% % Legenda tripla
+% leg1 = legend(D3,{'Separável', 'Não-separável'},'Location','northwest','Orientation','horizontal', 'Color','none', 'EdgeColor','none');
+% set(leg1, 'FontSize', 50);
+% ah1=axes('position',get(gca,'position'),'visible','off');
+% leg2 = legend(ah1, D2,{'Separável', 'Não-separável'},'Location','southwest','Orientation','horizontal', 'Color','none', 'EdgeColor','none');
+% ah2=axes('position',get(gca,'position'),'visible','off');
+% set(leg2, 'FontSize', 50,'FontName', 'Times New Roman');
+% leg3 = legend(ah2,D1,{'Separável', 'Não-separável'},'Location','southwest','Orientation','horizontal', 'Color','none', 'EdgeColor','none');
+% set(leg3, 'FontSize', 50, 'FontName', 'Times New Roman');
